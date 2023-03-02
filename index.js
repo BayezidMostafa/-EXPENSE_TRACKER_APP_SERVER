72abfd0911ce55c785bcd7ffbcfcd11564af933f
Bayezid Mostafa <hiperx1@gmail.com>
2023-03-02 06:58:57 +0600

    DB Connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -6,6 +7,21 @@ const port = process.env.PORT || 5000;
 // Default middleware
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+const run = async () => {
+  try {
+  } catch {
+  } finally {
+  }
+};
+run().catch((err) => console.error(err.message));
 
 app.get("/", (req, res) => {
   res.send("Server is running");
